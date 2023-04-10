d4a8b969d81edd279d4f9898c5b42aa4de87a866
Neelesh2909 <rawat.neelesh9@gmail.com>
2023-04-10 10:42:48 +0530

    [Neelesh] Add: present or absent

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,1 +1,10 @@
+#!/bin/bash -x
 echo "Hello to employee wage computation problem"
+isPresent=1
+empCheck=$((RANDOM%2))
+if [[ isPresent -eq empCheck ]]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
